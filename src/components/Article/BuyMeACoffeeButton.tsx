6269dd707bdc1b74523e006d290d46de3df8b72d
Killinsun - Ryota Takeuchi <noneck.0311@gmail.com>
2025-10-28 06:40:28 +0900

    Buy me a coffee ボタンをコンポーネントにする #11

---
+++ src/components/Article/BuyMeACoffeeButton.tsx
@@ -0,0 +1,8 @@
+export const BuyMeACoffeeButton = () => {
+	return (
+		<div className="flex flex-col items-center">
+			<p>キリンさんにコーヒーを奢る</p>
+			<a href="https://www.buymeacoffee.com/noneck0311f"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=noneck0311f&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" /></a>
+		</div>
+	)
+}
